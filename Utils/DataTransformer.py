9111fbb791a2ec76887641cf7cc19fde1e87928c
Jixian Wang <jixian@shadowgrid.io>
2017-02-25 17:07:23 +0100

    测试了一下根目录读取

---
+++ Utils/DataTransformer.py
@@ -0,0 +1,2 @@
+import config
+print (config.PROJECT_ROOT)
--- test_stock_factors.py
+++ test_stock_factors.py
@@ -3,6 +3,7 @@ import numpy as np
 from datetime import date
 from ML.Model5M_T1 import Model5MT1
 from time import clock
+import config
 
 MODEL_NAME = 'model5m-t1-1.h5'
 # 训练代码
@@ -11,7 +12,7 @@ START_DATE = date(2015, 12, 8)
 END_DATE = date(2016, 1, 5)
 
 m5m = Model5MT1(MODEL_NAME)
-
+print(config.PROJECT_ROOT)
 
 start_ts = clock()
 X, y = m5m.prepare_data('sh600108', START_DATE, END_DATE, use_cache=False)
