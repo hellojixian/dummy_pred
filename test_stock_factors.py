f8c625029bbb70dd164855b47c174920a419d8e8
Jixian Wang <jixian@shadowgrid.io>
2017-02-25 02:32:32 +0100

    增加计时器代码

--- test_stock_factors.py
+++ test_stock_factors.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 from datetime import date
 from ML.Model5M_T1 import Model5MT1
+from time import clock
 
 MODEL_NAME = 'model5m-t1-1.h5'
 # 训练代码
@@ -11,6 +12,11 @@ END_DATE = date(2016, 1, 5)
 
 m5m = Model5MT1(MODEL_NAME)
 
+
+start_ts = clock()
 X, y = m5m.prepare_data('sh600108', START_DATE, END_DATE, use_cache=False)
+finish_ts = clock()
+print("execution time: {:10.6} s".format(finish_ts - start_ts))
+
 
 exit(0)
