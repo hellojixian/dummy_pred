dd27a90de90d2b4876b2bc29846f84c38e989c56
Jixian Wang <jixian@shadowgrid.io>
2017-02-25 02:24:39 +0100

    修个DMA的小错误

--- FeatureExtractor/DMA.py
+++ FeatureExtractor/DMA.py
@@ -19,7 +19,6 @@ def calculate(df):
 
     idx_close = df.columns.get_loc('close')
     for i in range(0, df.shape[0]):
-        close = df.iloc[i, idx_close]
         ma_n1 = float(np.mean(df.iloc[i - n1:i, idx_close]))
         ma_n2 = float(np.mean(df.iloc[i - n2:i, idx_close]))
         dif = ma_n1 - ma_n2
@@ -29,6 +28,6 @@ def calculate(df):
     idx_dif = df.columns.get_loc('dma_dif')
     for i in range(0, df.shape[0]):
         ama = float(np.mean(df.iloc[i - m:i, idx_dif]))
-        ama_list.append(dif)
+        ama_list.append(ama)
     df['dma_ama'] = pd.Series(ama_list, index=df.index)
     return df
