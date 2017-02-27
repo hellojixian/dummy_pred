f69b042524d5ab60d35f0cb2fd3a7063e96d3076
Jixian Wang <jixian@shadowgrid.io>
2017-02-27 07:55:16 +0100

    update

--- test_stock_factors.py
+++ test_stock_factors.py
@@ -8,7 +8,7 @@ import Common.Visualization as v
 
 MODEL_NAME = 'model5m-t1-1.h5'
 # 训练代码
-
+STOCK_CODE = 'sh600088'
 START_DATE = date(2013, 12, 20)
 END_DATE = date(2016, 1, 5)
 
@@ -16,12 +16,12 @@ m5m = Model5MT1(MODEL_NAME)
 print(config.PROJECT_ROOT)
 
 start_ts = clock()
-X, y = m5m.prepare_data('sh600088', START_DATE, END_DATE, use_cache=True)
+X, y = m5m.prepare_data(STOCK_CODE, START_DATE, END_DATE, use_cache=True)
 finish_ts = clock()
 print("\nExecution time: {:10.6} s".format(finish_ts - start_ts))
 
 print(X[1].shape)
-v.animate_data(X)
-
+labels = m5m.data_features();
+v.animate_data(X, labels, STOCK_CODE)
 
 exit(0)
